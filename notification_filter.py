b2506b2cdc13a46fd39fea6b3ba7e94d03ce60bc
omert <omer@parametrixinsurance.com>
2021-08-31 18:32:21 +0300

    Did stuff with Kafka, it is most likely wrong

--- notification_filter.py
+++ notification_filter.py
@@ -6,74 +6,56 @@ from botocore.client import Config
 from json import loads
 from kafka import KafkaConsumer
 from kafka.admin import KafkaAdminClient, NewTopic
+import topic_test
 
 
-def put_object(filename):
-    client = boto3.client('s3')
+def put_object(message):
+    region = message['awsRegion']
+    account_id = message['id']
+    resource = '*'  # Not sure about this
+    bucket_name = message['s3']['bucket']['name']
+    filename = ""  # Where is it?
+    client = boto3.client(f'arn:aws:s3:{region}:{account_id}:accesspoint/{resource}')
     #  Save to S3
-    ans = client.upload_file(Filename=filename, Bucket=bucketname,
+    ans = client.upload_file(Filename=filename, Bucket=bucket_name,
                              Key=filename)
-    return ans
+    if ans:
+        print(f'Object {filename} put in {bucket_name} successfully')
 
 
-if len(sys.argv) != 5:
-    print('Usage: ' + sys.argv[0] + ' [region name] <bucket> <topic name> <notification Id>')
-    sys.exit(1)
-
-region_name = sys.argv[1]
-bucketname = sys.argv[2]
-topic_name = sys.argv[3]
-notification_id = sys.argv[4]
+topic_conf_list = [{'Id': 'shtut',
+                    'TopicArn': topic_test.main(),
+                    'Events': ['s3:ObjectSynced:*'],
+                    }]
 
 # endpoint and keys from vstart
 endpoint = 'http://127.0.0.1:8000'
 access_key = '0555b35654ad1656d804'
 secret_key = 'h7GhxuBLTrlhVUyxSPUKUV8r/2EI4ngqJxD7iBdBYLhwluN30JaT3Q=='
 
-# TODO: Replace with Kafka connection
 
-admin_client = KafkaAdminClient(
-    bootstrap_servers=endpoint,
-    client_id=access_key
-)
-
-admin_client.create_topics(new_topics=[topic_name], validate_only=False)
-
+# Create new Kafka consumer to listen to the message from Ceph
 consumer = KafkaConsumer(
-    topic_name,
+    "topic_name",
     bootstrap_servers=endpoint,
     auto_offset_reset='earliest',
     enable_auto_commit=True,
-    group_id=notification_id,
+    group_id=0xF000,
     value_deserializer=lambda x: loads(x.decode('utf-8')))
 
 print(consumer)
 
-topic_arn = new_topic['CreateTopicResponse']['CreateTopicResult']['TopicArn']
+consumer_list = [message.value for message in consumer]
+
+# Put objects to the relevant bucket
+for message in consumer_list:
+    put_object(message)
+
+
 
 # regex filter on the object name and metadata based filtering are extension to AWS S3 API
 # bucket and topic should be created beforehand
 
-topic_conf_list = [{'Id': notification_id,
-                    'TopicArn': topic_arn,
-                    'Events': ['s3:ObjectSynced:*'],
-                    'Filter': {
-                        # 'Metadata': {
-                        #     'FilterRules': [{'Name': 'x-amz-meta-foo', 'Value': 'bar'},
-                        #                     {'Name': 'x-amz-meta-hello', 'Value': 'world'}]
-                        # },
-                        # 'Key': {
-                        #     'FilterRules': [{'Name': 'regex', 'Value': '([a-z]+)'}]
-                        # }
-                    }}]
 
-print(client.put_bucket_notification_configuration(Bucket=bucketname,
-                                                   NotificationConfiguration={'TopicConfigurations': topic_conf_list}))
 
-# TODO: Put object
-filename = "blabla.jpeg"
-obj_put = put_object(filename)
 
-# TODO: Kafka notification through consumer
-if obj_put:
-    print(f'Object {filename} put in {bucketname} successfully')
--- topic_test.py
+++ topic_test.py
@@ -2,40 +2,41 @@
 
 import boto3
 import sys
-from urllib.parse import parse_qsl
 from botocore.client import Config
 
-if len(sys.argv) == 3:
-    # topic name as first argument
-    topic_name = sys.argv[1]
-    # region name as second argument
-    region_name = sys.argv[2]
-elif len(sys.argv) == 2:
-    # topic name as first argument
-    topic_name = sys.argv[1]
-    region_name = ""
-else:
-    print('Usage: ' + sys.argv[0] + ' <topic name> [region name]')
-    sys.exit(1)
 
-# endpoint and keys from vstart
-endpoint = 'http://127.0.0.1:8000'
-access_key = '0555b35654ad1656d804'
-secret_key = 'h7GhxuBLTrlhVUyxSPUKUV8r/2EI4ngqJxD7iBdBYLhwluN30JaT3Q=='
+def main():
+    if len(sys.argv) == 3:
+        # topic name as first argument
+        topic_name = sys.argv[1]
+        # region name as second argument
+        region_name = sys.argv[2]
+    elif len(sys.argv) == 2:
+        # topic name as first argument
+        topic_name = sys.argv[1]
+        region_name = ""
+    else:
+        print('Usage: ' + sys.argv[0] + ' <topic name> [region name]')
+        sys.exit(1)
 
-client = boto3.client('sns',
-                      endpoint_url=endpoint,
-                      aws_access_key_id=access_key,
-                      region_name=region_name,
-                      aws_secret_access_key=secret_key,
-                      config=Config(signature_version='s3'))
+    # endpoint and keys from vstart
+    endpoint = 'http://127.0.0.1:8000'
+    access_key = '0555b35654ad1656d804'
+    secret_key = 'h7GhxuBLTrlhVUyxSPUKUV8r/2EI4ngqJxD7iBdBYLhwluN30JaT3Q=='
 
-# to see the list of available "regions" use:
-# radosgw-admin realm zonegroup list
+    client = boto3.client('sns',
+                          endpoint_url=endpoint,
+                          aws_access_key_id=access_key,
+                          region_name=region_name,
+                          aws_secret_access_key=secret_key,
+                          config=Config(signature_version='s3'))
 
-# this is standard AWS services call, using custom attributes to add AMQP endpoint information to the topic
+    # to see the list of available "regions" use:
+    # radosgw-admin realm zonegroup list
 
-endpoint_args = 'push-endpoint=amqp://127.0.0.1:5672&amqp-exchange=ex1&amqp-ack-level=broker'
-attributes = {nvp[0]: nvp[1] for nvp in parse_qsl(endpoint_args, keep_blank_values=True)}
+    # this is standard AWS services call, using custom attributes to add Kafka endpoint information to the topic
+    ans = client.create_topic(Name="Kafka_Broker", Attributes={"push-endpoint": 'kafka://127.0.0.1'})
 
-print(client.create_topic(Name=topic_name, Attributes=attributes))
+    return ans
+
+print(main())
